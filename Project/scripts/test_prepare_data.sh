4c69650d20fed457b3aa7a4a653fc6e695dae85c
Daniel Munro <danbmunro@gmail.com>
2022-11-14 14:41:04 -0600

    Update test data

--- Project/scripts/test_prepare_data.sh
+++ Project/scripts/test_prepare_data.sh
@@ -1,24 +1,54 @@
 # These are the commands used to generate the small test dataset, which
-# consists of 10 randomly chosen Geuvadis samples, subsetted to the first
-# Mb of chr1, with reference files subsetted accordingly.
+# consists of 16 randomly chosen Geuvadis samples, subsetted to the first
+# 2 Mb of chr1, with reference files subsetted accordingly.
 
-## Subset fastq and make new fastq mapping file for them
-python3 src/test_subset_fastq.py
+#################
+## Subset data ##
+#################
+
+## Get sample subset
+geno="~/bulk/pantry/Geuvadis/genotype/GEUVADIS.445_samples.GRCh38.20170504.maf01.filtered.nodup"
+mkdir -p input
+shuf -n 16 $geno.fam | cut -f2 | sort > input/samples.txt
+
+## Subset bam files
+mkdir -p input/bam
+while read f; do
+    echo $f
+    samtools view ../../Geuvadis/intermediate/bam/$f.Aligned.sortedByCoord.out.bam "1:1-2000000" -b > input/bam/$f.bam
+done < input/samples.txt
+
+## Make BAM map
+awk '{print $1".bam\t"$1}' input/samples.txt > input/bam_map.txt
+
+## Subset genotypes (Add 1 Mb to include the full cis-window for the test region genes)
+genonew="../Pheast/input/Geuvadis.16_samples.GRCh38.chr1_0-2Mb"
+mkdir -p ../Pheast/input
+plink2 --bfile $geno \
+    --chr 1 \
+    --from-mb 0 \
+    --to-mb 3 \
+    --make-bed \
+    --out $genonew
+
+########################
+## Subset annotations ##
+########################
 
 ## Subset gene annotations
-mkdir -p test/ref
-gtf="../data/human_ref/Homo_sapiens.GRCh38.106.gtf"
-gtfnew="test/ref/Homo_sapiens.GRCh38.106.chr1_0-1Mb.gtf"
+mkdir -p input/ref
+gtf="~/bulk/pantry/Geuvadis/human_ref/Homo_sapiens.GRCh38.106.gtf"
+gtfnew="input/ref/Homo_sapiens.GRCh38.106.chr1_0-2Mb.gtf"
 head -5 "$gtf" > "$gtfnew"
-awk '($1 == "1") && ($4 < 1000000) && ($5 < 1000000)' "$gtf" >> "$gtfnew"
+awk '($1 == "1") && ($4 < 2000000) && ($5 < 2000000)' "$gtf" >> "$gtfnew"
 
 ## Subset retroelement annotations
-retro="../data/human_ref/retro.hg38.v1.nochr.gtf"
-retronew="test/ref/retro.hg38.v1.nochr.chr1_0-1Mb.gtf"
-awk '($1 == "1") && ($4 < 1000000) && ($5 < 1000000)' "$retro" > "$retronew"
+retro="~/bulk/pantry/Geuvadis/human_ref/retro.hg38.v1.nochr.gtf"
+retronew="input/ref/retro.hg38.v1.nochr.chr1_0-2Mb.gtf"
+awk '($1 == "1") && ($4 < 2000000) && ($5 < 2000000)' "$retro" > "$retronew"
 
 ## Subset genome
-fa="../data/human_ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa"
-fanew="test/ref/Homo_sapiens.GRCh38.dna.primary_assembly.chr1_0-1Mb.fa"
-# 60 bases per line, so first Mb is in 16667 lines
-head -16668 "$fa" > "$fanew"
+fa="~/bulk/pantry/Geuvadis/human_ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa"
+fanew="input/ref/Homo_sapiens.GRCh38.dna.primary_assembly.chr1_0-2Mb.fa"
+# 60 bases per line, so first 2 Mb is 33334 lines
+head -33335 "$fa" > "$fanew"
