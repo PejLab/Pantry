1f6f5c18f9902a5c47e7c716e8e99a3a4a447b3e
daniel-munro <danbmunro@gmail.com>
2026-03-02 18:52:54 -0800

    Update Pheast test data
    Include all modalities by default. Reduce example genotypes from full chr1 to chr1 first 120 Mb.

--- pheast/scripts/test_subset_grouped_pheno.py
+++ pheast/scripts/test_subset_grouped_pheno.py
@@ -11,22 +11,16 @@ parser.add_argument("--chrom", type=str, help="Subset to phenotypes on this chro
 parser.add_argument("--n-groups", type=int, help="Number of groups to keep")
 args = parser.parse_args()
 
-# df = pd.read_csv(args.in_bed, sep='\t', index_col='phenotype_id', dtype={'#chr': str, 'start': int, 'end': int})
 df = pd.read_csv(args.in_bed, sep='\t', dtype={'#chr': str, 'start': int, 'end': int})
-# groups = pd.read_csv(args.in_groups, sep='\t', names=['phenotype_id', 'gene_id'], index_col='phenotype_id')
 groups = pd.read_csv(args.in_groups, sep='\t', names=['phenotype_id', 'gene_id'])
 
 if args.chrom:
     df = df[df['#chr'] == args.chrom]
-    # groups = groups.loc[df.index]
     groups = groups[groups['phenotype_id'].isin(df['phenotype_id'])]
 
 if args.n_groups:
-    # subset groups to N groups:
     keep = groups['gene_id'].unique()[:args.n_groups]
     groups = groups[groups['gene_id'].isin(keep)]
-    # subset df to those in groups:
-    # df = df.loc[groups.index]
     df = df[df['phenotype_id'].isin(groups['phenotype_id'])]
 
 df.to_csv(args.out_bed, sep='\t', index=False)
